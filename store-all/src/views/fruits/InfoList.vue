f3cc4ff28cf5aadd31a9a99fecc20ea65eae2e41
lirf2018 <42826784+lirf2018@users.noreply.github.com>
2021-02-18 20:26:25 +0800

    66

---
+++ store-all/src/views/fruits/InfoList.vue
@@ -0,0 +1,176 @@
+<template>
+	<div class="body-bg">
+		<div><Head :title="title" /></div>
+		<van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+			<div class="activity-list" v-for="(item, index) in infoList" :key="item.infoId">
+				<div>
+					<div class="activity-title">
+						<div>
+							<span class="title">{{ item.infoTitle }}</span>
+							<span class="is-new" v-show="item.activityIsNew">new</span>
+						</div>
+						<div>
+							<span>{{ item.endTime }} 结束</span>
+						</div>
+					</div>
+					<div class="activity-img">
+						<div><img :src="item.infoImg" /></div>
+					</div>
+					<div class="activity-desc">
+						<span>{{ item.markMsg }}</span>
+					</div>
+					<div class="activity-detail" @click="toDetailPage(item.infoId)">
+						<div>
+							<span>查看详情</span>
+							<span><van-icon name="arrow" /></span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</van-list>
+	</div>
+</template>
+
+<script>
+import Head from '@/components/Head.vue';
+import axios from '@/network/request.js';
+export default {
+	components: { Head: Head },
+	data() {
+		return {
+			title: '资讯',
+			infoList: [],
+			loading: false,
+			finished: false,
+			testId: 0,
+			currePage: 1,
+			hasNext: false
+		};
+	},
+	methods: {
+		onLoad() {
+			let vm = this;
+			let params = {
+				req_type: 'query_info_page',
+				data: {
+					currePage: vm.currePage,
+					userId: 0
+				}
+			}; // 参数
+			axios.post('', params).then(function(res) {
+				if (res.resp_code == 1) {
+					let dataList = res.data.list;
+					let hasNext = res.data.hasNext;
+					vm.initDataList(dataList, hasNext);
+					vm.currePage = vm.currePage + 1;
+				} else {
+				}
+			});
+		},
+		initDataList(dataList, hasNext) {
+			let vm = this;
+			setTimeout(() => {
+				for (let i = 0; i < dataList.length; i++) {
+					var obj = {
+						infoId: dataList[i].infoId,
+						infoTitle: dataList[i].infoTitle,
+						endTime: dataList[i].endTime,
+						activityIsNew: true,
+						infoImg: dataList[i].infoImg,
+						markMsg: dataList[i].markMsg
+					};
+					vm.infoList.push(obj);
+				}
+				vm.loading = false;
+				if (hasNext == false) {
+					vm.finished = true;
+				}
+			}, 1000);
+		},
+		toDetailPage(infoId) {
+			let url = '/infoDetail?infoId=' + infoId;
+			this.$router.push(url);
+			//打开新页签
+			//const page  = this.$router.resolve({path: url})
+			//window.open(page.href,'_blank')
+		}
+	}
+};
+</script>
+
+<style scoped>
+.body-bg {
+	border: none;
+	line-height: 20px;
+	margin: 0;
+	padding: 0;
+	font-size: 14px;
+	color: #323233;
+	font-family: Avenir, PingFang SC, Arial, Helvetica, STHeiti STXihei, Microsoft YaHei, Tohoma, sans-serif;
+}
+.activity-list {
+	background-color: #ffffff;
+	margin: 10px 10px;
+	color: #657180;
+}
+.activity-list > div {
+	padding: 10px 10px;
+}
+
+.activity-title {
+	padding: 5px 0;
+	padding-bottom: 10px;
+}
+
+.activity-title > div:first-child {
+	float: left;
+	padding-bottom: 10px;
+	font-weight: bold;
+}
+
+.is-new {
+	font-weight: normal;
+	font-size: 12px;
+	background-color: #008000;
+	color: #ffffff;
+	padding: 0 3px;
+	margin-left: 3px;
+}
+
+.activity-title > div:last-child {
+	float: right;
+	padding-bottom: 10px;
+	font-size: 12px;
+}
+
+.activity-img img {
+	width: 100%;
+	border-radius: 10px;
+}
+
+.activity-desc {
+	font-size: 12px;
+	padding: 5px 0 10px 0;
+}
+
+.activity-detail {
+	font-size: 12px;
+	overflow: hidden;
+	border-top: 1px solid #f0f0f0;
+	padding-top: 10px;
+}
+
+.activity-detail > div > span {
+	display: block;
+	float: left;
+	width: 50%;
+}
+
+.activity-detail > div > span:first-child {
+	color: #008000;
+}
+.activity-detail > div > span:last-child {
+	text-align: right;
+	font-size: 14px;
+}
+</style>
---
+++ store-all/src/views/other/Number.vue
@@ -0,0 +1,178 @@
+<template>
+	<div class="app-div">
+		<div class="in-number"><input v-model="vlaue" @keydown="insertNumber" @keyup="insertNumber" /></div>
+		<div class="div-1"></div>
+		<div class="numbe-panel">
+			<div>
+				<div :class="item.numbePanelClass" v-for="(item, index) in numbesList" :key="index">
+					<span :class="item.fontColor">{{ item.numStr }}</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			vlaue: null,
+			numbesList: []
+		};
+	},
+	mounted() {
+		let vm = this;
+		for (var i = 1; i < 50; i++) {
+			let key = i;
+			if (i < 10) {
+				key = 0 + '' + i;
+			}
+			let obj = {
+				numStr: key,
+				numbePanelClass: 'numbe-panel-no-unchose',
+				fontColor: 'fontColor-green'
+			};
+			vm.setFontColor(obj);
+			vm.numbesList.push(obj);
+		}
+	},
+	methods: {
+		insertNumber() {
+			let vm = this;
+			//
+			for (let j = 0; j < vm.numbesList.length; j++) {
+				// 取第二个位数
+				let obj = vm.numbesList[j].numStr + '';
+				//
+				let ket_ = obj.charAt(1) + '';
+				vm.numbesList[j].numbePanelClass = 'numbe-panel-no-unchose';
+			}
+			// 选中的下标
+			let cheose = [];
+			if (vm.vlaue != null && vm.vlaue != '') {
+				for (let i = 0; i < vm.vlaue.length; i++) {
+					let key = vm.vlaue.charAt(i) + '';
+					for (let j = 0; j < vm.numbesList.length; j++) {
+						// 取第二个位数
+						let obj = vm.numbesList[j].numStr + '';
+						//
+						let ket_ = obj.charAt(1) + '';
+						if (key == ket_) {
+							cheose.push(j);
+						}
+					}
+				}
+				// 设置选中
+				for (var i = 0; i < cheose.length; i++) {
+					vm.numbesList[cheose[i]].numbePanelClass = 'numbe-panel-no-chose';
+				}
+			}
+		},
+		setFontColor(data) {
+			if (
+				data.numStr == '01' ||
+				data.numStr == '02' ||
+				data.numStr == '07' ||
+				data.numStr == '08' ||
+				data.numStr == '13' ||
+				data.numStr == '19' ||
+				data.numStr == '12' ||
+				data.numStr == '18' ||
+				data.numStr == '23' ||
+				data.numStr == '24' ||
+				data.numStr == '29' ||
+				data.numStr == '30' ||
+				data.numStr == '34' ||
+				data.numStr == '35' ||
+				data.numStr == '40' ||
+				data.numStr == '45' ||
+				data.numStr == '46'
+			) {
+				//红色
+				data.fontColor ='fontColor-red'
+			} else if (
+				data.numStr == '03' ||
+				data.numStr == '04' ||
+				data.numStr == '09' ||
+				data.numStr == '10' ||
+				data.numStr == '14' ||
+				data.numStr == '15' ||
+				data.numStr == '20' ||
+				data.numStr == '25' ||
+				data.numStr == '26' ||
+				data.numStr == '31' ||
+				data.numStr == '37' ||
+				data.numStr == '36' ||
+				data.numStr == '42' ||
+				data.numStr == '41' ||
+				data.numStr == '47' ||
+				data.numStr == '48'
+			) {
+				//蓝色
+				data.fontColor ='fontColor-blue'
+			}
+		}
+	}
+};
+</script>
+
+<style scoped>
+.div-1 {
+	height: 20px;
+	background-color: #fbffa6;
+}
+.in-number {
+	text-align: center;
+	margin: 0px auto;
+	background-color: #fbffa6;
+}
+
+.in-number > input {
+	border: 1px solid grey;
+	margin-top: 20px;
+	height: 30px;
+	font-size: 18px;
+	font-weight: bold;
+}
+.numbe-panel {
+	background-color: #fbffa6;
+	margin: 0 auto;
+	text-align: center;
+	overflow: auto;
+}
+.numbe-panel-no-unchose {
+	border: 3px solid 	#C0C0C0;
+	width: 35px;
+	height: 35px;
+	line-height: 35px;
+	text-align: center;
+	border-radius: 50% 50%;
+	float: left;
+	background-color: white;
+	margin: 20px 0 0 10px;
+	font-size: 18px;
+}
+
+.numbe-panel-no-chose {
+	border: 3px solid red;
+	width: 35px;
+	height: 35px;
+	line-height: 35px;
+	text-align: center;
+	border-radius: 50% 50%;
+	float: left;
+	margin: 20px 0 0 10px;
+	background-color: white;
+	font-size: 18px;
+}
+
+.fontColor-red {
+	color: 	#DC143C;
+}
+.fontColor-green {
+	color: 	#32CD32;
+}
+.fontColor-blue {
+	color: #6495ED;
+}
+</style>
